487bf1bcc3b9e97b32f2475ac6a647e1fb8fcc78
Becelydada52 <accauntontv52@gmail.com>
2025-05-06 17:23:29 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -14,6 +14,18 @@ def main():
 def price():
     return render_template('Price.html')
 
+@app.route('/price1')
+def price1():
+    return render_template('price1.html')
+
+@app.route('/price2')
+def price2():
+    return render_template('price2.html')
+
+@app.route('/price3')
+def price3():
+    return render_template('price3.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
